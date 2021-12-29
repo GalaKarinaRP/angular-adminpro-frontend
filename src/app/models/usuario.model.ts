0578c9f10335f85f1c7acff12a5ea3ab5604eb6a
Gala Karina Reyes Peña <galakarinareyes@gmail.com>
2021-12-29 12:14:39 -0600

    fin seccion 15

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,35 @@
+import { environment } from "src/environments/environment";
+
+const base_url = environment.base_url;
+
+export class Usuario {
+
+    constructor(
+        public nombre: string,
+        public email: string,
+        public password?: string,
+        public img?: string,
+        public google?: boolean,
+        public role?: string,
+        public uid?: string,
+    ) {}
+
+   get imagenUrl(){
+    // localhost:3000/api/upload/usuario/no-image
+  
+        if( this.img.includes('https') )
+        {
+            return this.img;
+        }
+
+        if(this.img){
+            return `${ base_url }/upload/usuarios/${this.img}`;
+        }
+        else{
+            return `${ base_url }/upload/usuarios/no-image`;
+        }   
+      
+   }
+
+}
+
---
+++ src/app/pages/perfil/perfil.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { FileUploadService } from 'src/app/services/file-upload.service';
+
+
+import { Usuario } from 'src/app/models/usuario.model';
+
+
+@Component({
+  selector: 'app-perfil',
+  templateUrl: './perfil.component.html',
+  styles: [
+  ]
+})
+export class PerfilComponent implements OnInit {
+
+  public perfilForm:FormGroup;
+  public usuario:Usuario;
+  public imagenSubir:File;
+  public imgTemp: any = null;
+
+  constructor( private fb:FormBuilder, 
+    private usuarioService: UsuarioService,
+    private fileUploadService: FileUploadService) {
+
+      this.usuario = usuarioService.usuario;
+
+     }
+
+  ngOnInit(): void {
+    this.perfilForm = this.fb.group({
+      nombre:[this.usuario.nombre, Validators.required],
+      email: [this.usuario.email,[ Validators.required, Validators.email]]
+    });
+  }
+
+  actualizarPerfil(){
+    console.log(this.perfilForm.value);
+    this.usuarioService.actualizarUsuario(this.perfilForm.value)
+    .subscribe( () => {      
+      const { nombre, email } = this.perfilForm.value;
+      this.usuario.nombre = nombre;
+      this.usuario.email = email;    
+      
+      Swal.fire('Guardado','Cambios guardados,','success');
+    }, (err) => {
+      Swal.fire('Error', err.error.msg , 'error');
+      console.log(err.error.msg);
+      
+    });
+  }
+
+  cambiarImagen( file: File){
+   this.imagenSubir = file;
+   if(!file){ 
+     return this.imgTemp = null;
+    }
+
+   const reader = new FileReader();   
+
+   reader.onloadend = () =>{
+     this.imgTemp = reader.result;
+     console.log( reader.result);     
+   }
+    
+  }
+
+  subirImagen(){
+    this.fileUploadService.actualizarFoto(this.imagenSubir, 'usuarios',this.usuario.uid )
+    .then( img  => {
+      this.usuario.img = img;
+      Swal.fire('Guardado','Imagen de usuario actualizada,','success');    
+    }).catch( err => {
+      Swal.fire('Guardado','No se pudo subir la imagen,','success');
+      console.log(err);
+      
+    });
+  }
+
+}
